ced4b7376f414b350d3b36935d17126d6ad97f6e
Saadat Ali <saadataliklasra33@gmail.com>
2023-06-08 10:58:57 +0500

    fixed pagination and filtration

--- core/main/templatetags/custom_tags.py
+++ core/main/templatetags/custom_tags.py
@@ -8,3 +8,13 @@ register = template.Library()
 def get_proper_elided_page_range(p, number, on_each_side=3, on_ends=2):
     paginator = Paginator(p.object_list, p.per_page)
     return paginator.get_elided_page_range(number=number, on_each_side=on_each_side, on_ends=on_ends)
+
+
+@register.simple_tag(takes_context=True)
+def param_replace(context, **kwargs):
+    d = context["request"].GET.copy()
+    for k, v in kwargs.items():
+        d[k] = v
+    for k in [k for k, v in d.items() if not v]:
+        del d[k]
+    return d.urlencode()
